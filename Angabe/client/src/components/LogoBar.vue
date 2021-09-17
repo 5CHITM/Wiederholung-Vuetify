0ebdb3d5de600b42268c6b1e9fb54ced42091015
OnSebii <lang.sebastian.006@gmail.com>
2021-09-17 14:32:09 +0200

    Update LogoBar.vue

--- Angabe/client/src/components/LogoBar.vue
+++ Angabe/client/src/components/LogoBar.vue
@@ -1,7 +1,13 @@
 <template>
-  <v-container>
-    <img> 
-  </v-container>
+  <v-app>
+    <v-main>
+      <template>
+        <v-card class="overflow-hidden">
+          <v-app-bar absolute dark shrink-on-scroll prominent src="../../public/logo.png"> </v-app-bar>
+        </v-card>
+      </template>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
